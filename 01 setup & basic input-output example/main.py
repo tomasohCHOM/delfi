de3c4a32fc759d33407f32432f3d807623f3e246
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-11 19:42:42 -0700

    Files setup for lesson 1

---
+++ 01 setup & basic input-output example/main.py
@@ -0,0 +1,4 @@
+# This represents a comment. The purpose of a comment is to express
+# your thoughts to potential future code-readers. Anything that goes
+# after a "#" is considered a comment in Python. In other programming
+# languages (like Java), those may be "//"
