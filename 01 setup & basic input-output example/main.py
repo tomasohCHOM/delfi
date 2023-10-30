54b4057852ba1a0f89c6fb6786b7a98106078b8e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-30 16:31:50 -0700

    Finished lesson 1 - user input and output + basic arithmetic

--- 01 setup & basic input-output example/main.py
+++ 01 setup & basic input-output example/main.py
@@ -2,3 +2,14 @@
 # your thoughts to potential future code-readers. Anything that goes
 # after a "#" is considered a comment in Python. In other programming
 # languages (like Java), those may be "//"
+
+print("Hello World!")  # This will print "Hello World!" in the terminal
+
+print("Give me your name:")
+name = input()
+
+print("Your name is: " + name)
+
+my_int = int(input("Please pick a number: "))  # int() convers a string to a number
+print("Your number is " + str(my_int))  # str() converts to string
+print("Your number plus 3 is equal to: " + str(my_int + 3))
