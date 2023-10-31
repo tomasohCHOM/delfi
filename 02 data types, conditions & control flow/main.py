cb01e7700f401ca6fd99ecc6d66a0b3078dfd31e
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-10-31 08:52:33 -0700

    Created setup for lesson 2

---
+++ 02 data types, conditions & control flow/main.py
@@ -0,0 +1,3 @@
+# This lesson is all about understanding different data types and how they can be
+# used for control flow in Python programs. We will discuss if-statements and conditions,
+# and we will build a simple rock-paper-scissors game using this knowledge.
